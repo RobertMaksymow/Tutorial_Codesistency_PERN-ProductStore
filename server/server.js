8ea5674d29b1a3333aa69b664dcc86f651604c9a
Dell <robert.maksymow@tutanota.com>
2026-08-20 21:22:26 +0100

    before adding routes to API. T20min

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
 
+dotenv.config(); // Load environment variables from .env file
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // Middleware to parse incoming JSON requests
+app.use(cors()); // Middleware to enable Cross-Origin Resource Sharing (CORS)
 app.use(helmet()); // Security middleware to set various HTTP headers for security
 app.use(morgan("dev")); // Logging middleware to log HTTP requests
 
@@ -11,6 +17,6 @@ app.get("/", (req, res) => {
   res.send("Hello World from the ROOT route!");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
