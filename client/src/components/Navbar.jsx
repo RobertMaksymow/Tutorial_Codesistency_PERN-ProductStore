1cd115127f24adc5db9ce93311299aa774820b5b
Dell <robert.maksymow@tutanota.com>
2026-09-18 23:00:12 +0100

    Navbar started. T1h.33min

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-base-100/80 backdrop-blur-large border-b border-base-content/10 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto">
+        <div className="navbar px-4 min-h-[4rem] justify-between">Navbar</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
